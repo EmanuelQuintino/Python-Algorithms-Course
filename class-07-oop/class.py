520830a2213cabbfe3c038fd612eb891a9e93961
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-05-06 22:13:10 -0300

    add class 07 oop

--- class-07-oop/class.py
+++ class-07-oop/class.py
@@ -1,30 +1,26 @@
-# OOP
-
 class Car:
-  def __init__(self, brand, model, year, fipe, offer):
+  def __init__(self, brand, model, year, fipe):
     self.brand = brand
     self.model = model
     self.year = year
-    self.offer = offer
     self.fipe = fipe
   
   def info(self):
-    return (self.brand, self.model, self.year)
+    return (self.brand, self.model, self.year, self.fipe)
   
-  def sale(self):
-    if self.offer >= self.fipe:
+  def sale(self, offer):
+    if offer >= self.fipe:
       return "Carro vendido!"
     else:
       return "Sai pra lá caloteiro!"
 
-myCar1 = Car("Toyota", "SW4", "2024", "345000", "350000")
-myCar2 = Car("Chevrolet", "Celta", "2006", "16500", "15000")
+car1 = Car("Toyota", "SW4", "2024", 345000)
+car2 = Car("Chevrolet", "Celta", "2006", 16500)
 
-print(myCar1.brand)
-print(myCar1.model)
-print(myCar1.year)
+print(car1.brand)
+print(car1.model)
+print(car1.year)
 
-print(myCar2.brand)
-print(myCar2.info())
-print(myCar2.sale())
-
+print(car2.brand)
+print(car2.info())
+print(car2.sale(15000))

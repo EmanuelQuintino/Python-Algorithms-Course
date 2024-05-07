e59daa0daf39ebf2f28c0920fa6b9fac4a0ec84a
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-05-07 20:54:01 -0300

    update class oop examples

--- class-07-oop/main.py
+++ class-07-oop/main.py
@@ -26,14 +26,25 @@ class Admin(User):
     self.__status = status 
 
   def get_status(self):
-    return self.__status 
+    return self.__status
   
   def info(self): # polymorphism
     return (self.name, self.get_cpf(), self.__status)
   
+  def reports(self):
+    if (self.__status == "gold"):
+      return {
+        "sale1": 1000,
+        "sale2": 2000,
+        "sale3": 3000,
+      }
+    else:
+      return "sem autorização!"
+
 admin = Admin("Natan", "111.111.111-11")
 print(admin.name)
 print(admin.get_status())
-admin.set_status("silver")
+admin.set_status("gold")
 print(admin.get_status())
 print(admin.info())
+print(admin.reports())

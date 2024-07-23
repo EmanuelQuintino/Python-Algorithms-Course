667c2ef0479e51d1e4d395451e33ce067f925e25
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-07-23 16:48:35 -0300

    update classes data types

---
+++ class-01-primitive-types/main.py
@@ -0,0 +1,29 @@
+print("Oi, Python!") # string
+print(10+20) # number
+
+# variable
+name = "Emanuel Quintino" 
+age = 37 
+
+print(name)
+print(age)
+
+print(type(name))
+print(type(age))
+
+num1 = 7
+num2 = 2
+
+print(num1 + num2)
+
+print(2 + int("3"))
+print("A" + "B")
+print("C" + "23")
+
+print("Eu sou " + name + " e tenho " + str(age) + " anos!")
+print(f"Eu sou {name} e tenho {age} anos!")
+
+is_admin = False #boolean True or False
+
+print(is_admin)
+print(type(is_admin))
--- class-02-structural-types/main.py
+++ class-02-structural-types/main.py
@@ -1,34 +1,3 @@
-# Primitive Types
-
-milk = "Leite Desnatado"
-volume = 2.5
-
-print(type(milk))
-print(type(volume))
-
-preparation1 = volume + 1
-preparation2 = milk + "10"
-
-print(preparation1)
-print(preparation2)
-
-life_cat = str(volume + 5)
-
-print("O gato bebe " + milk)
-print("E tem " + life_cat + " vidas")
-
-print("O gato bebe " + milk + " e tem " + life_cat + " vidas")
-print(f"O gato bebe {milk} e tem {life_cat} vidas")
-
-name = "Emanuel"
-age = 31
-is_admin = False #boolean True or False
-
-print(is_admin)
-print(type(is_admin))
-
-# Structural Types (Collections)
-
 animes = ["Dragon Ball", "Naruto", "Jojo", "Death Note"]
             # =>  0           1         2          3 
             # <= -4          -3        -2         -1

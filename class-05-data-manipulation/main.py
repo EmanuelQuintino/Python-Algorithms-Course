3a9962fdbc22b35f9e514da4cfa5eda4a715f12b
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-08-01 20:48:37 -0300

    update class data manipulation array

--- class-05-data-manipulation/main.py
+++ class-05-data-manipulation/main.py
@@ -43,13 +43,15 @@ print(name2)
 # arrays
 nums1 = [2, 3, 4]
 nums2 = [6, 7, 8]
+nums3 = nums1 + nums2
+
+print(nums3)
+print(max(nums3))
+print(min(nums3))
+print(4 in nums3)
+
 array_names = ["Ana", "João", "Letícia"]
 new_array_names = array_names.copy()
-
-print(nums1 + nums2)
-print(4 in nums1)
-print(max(nums1))
-print(min(nums2))
 
 array_names[0] = "Ana Clara"
 array_names.append("Natália")
@@ -58,6 +60,7 @@ array_names.extend(["João", "Carlos"])
 array_names.insert(1, "Cristian")
 new_array_names.pop()
 array_names.pop()
+array_names.pop(0)
 
 print(array_names.count("Ana Clara"))
 array_names.remove("Pedro")
